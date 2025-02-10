388c8d0fa3c3e8ae8b65c421781abd55c81d49bf
Rob Pataki <rob.pataki@icloud.com>
2025-02-10 22:24:01 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: true,
+  site: 'https://notes.onenterfra.me',
+  integrations: [
+    mdx({ syntaxHighlight: 'shiki', shikiConfig: { theme: 'tokyo-night' } }),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+  },
+})
